3ef7688cf66e7c579e2d60e7ff22c052353a3264
UriMtzF <39018208+UriMtzF@users.noreply.github.com>
2024-02-04 16:16:16 -0600

    Initial config for internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todone_app/generated/l10n.dart';
 import 'package:todone_app/pages/home.dart';
 
 void main() async {
@@ -14,6 +16,13 @@ class MyApp extends StatelessWidget {
         brightness: MediaQuery.platformBrightnessOf(context),
         seedColor: const Color.fromRGBO(0, 186, 186, 100));
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       title: 'ToDone App',
       theme: ThemeData(
         colorScheme: colorScheme,
